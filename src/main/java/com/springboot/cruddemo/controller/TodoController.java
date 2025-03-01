bbbb5ac76b492980096ba8618d4576a9a5599172
Rishib#321 <rishitab2209@gmail.com>
2025-03-01 15:47:00 +0530

    Implemented service layer and controllers for rest apis.

---
+++ src/main/java/com/springboot/cruddemo/controller/TodoController.java
@@ -0,0 +1,38 @@
+package com.springboot.cruddemo.controller;
+
+import com.springboot.cruddemo.dto.TodoDto;
+import com.springboot.cruddemo.entity.Todo;
+import com.springboot.cruddemo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    // Constructor Injection
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService=todoService;
+    }
+
+    // Build Add Todo REST API
+
+    @PostMapping
+    public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto) {
+
+        TodoDto savedTodo = todoService.addTodo(todoDto);
+
+        return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
+    }
+
+
+}
