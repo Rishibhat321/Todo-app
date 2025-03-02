e6321ca9f267fd9ec241230aad7ff766b3fa3584
Rishib#321 <rishitab2209@gmail.com>
2025-03-02 18:24:44 +0530

    Implemented service layer and controllers for rest apis, using model mapper, added custom exception handling.

--- src/main/java/com/springboot/cruddemo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/springboot/cruddemo/service/impl/TodoServiceImpl.java
@@ -81,4 +81,23 @@ public class TodoServiceImpl implements TodoService {
                 .collect(Collectors.toList());
     }
 
+
+    @Override
+    public TodoDto updateTodo(TodoDto todoDto, Long id) {
+
+       Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id- " + id));
+
+       todo.setTitle(todoDto.getTitle());
+       todo.setDescription(todoDto.getDescription());
+       todo.setCompleted(todoDto.isCompleted());
+
+       // save method performs both insert and update operation
+      Todo updatedTodo =  todoRepository.save(todo);
+
+      // convert updated jpa entity to todo dto
+        return modelMapper.map(updatedTodo, TodoDto.class);
+
+    }
+
 }
