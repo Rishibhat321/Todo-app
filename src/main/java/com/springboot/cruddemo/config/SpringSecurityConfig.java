6fcd4a14080206ac5e3472f3b27ace5b774ea346
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 13:14:11 +0530

    Implemented service layer and controllers for rest apis, using model mapper, added custom exception handling,configured spring security.

--- src/main/java/com/springboot/cruddemo/config/SpringSecurityConfig.java
+++ src/main/java/com/springboot/cruddemo/config/SpringSecurityConfig.java
@@ -7,11 +7,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,13 +37,13 @@ public class SpringSecurityConfig {
 
         UserDetails rishita = User.builder()
                 .username("rishita")
-                .password("password")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("admin")
+                .password(passwordEncoder().encode("password"))
                 .roles("ADMIN")
                 .build();
 
