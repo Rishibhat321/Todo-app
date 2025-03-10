589bf39078a40b9bdbea54fd95f328810b1ac24c
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 13:11:36 +0530

    Implemented service layer and controllers for rest apis, using model mapper, added custom exception handling,configured spring security.

--- src/main/java/com/springboot/cruddemo/config/SpringSecurityConfig.java
+++ src/main/java/com/springboot/cruddemo/config/SpringSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +23,23 @@ public class SpringSecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults());   // Enable HTTP Basic Authentication
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails rishita = User.builder()
+                .username("rishita")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(rishita, admin);
     }
 }
